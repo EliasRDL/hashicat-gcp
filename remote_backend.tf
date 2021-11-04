57309c858a29955ff79639c24bcf467d17167ea0
EliasRDL <eliasrdl@gmail.com>
2021-11-04 16:26:12 +0000

    Added vpc module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "EliasRDLCorp_NEW"
     workspaces {
       name = "hashicat-gcp"
