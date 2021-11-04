c8b8fcd70348cdd75043542f3cd1ace391f3c0ff
EliasRDL <eliasrdl@gmail.com>
2021-11-04 17:54:23 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,13 @@
 module "network" {
   source       = "terraform-google-modules/network/google"
   version      = "3.4.0"
+  project_id   = "p-8c8hwgj2m2fyicbmoc6t1a1pg30t"
   network_name = "gaurav-network"
-  project_id   = var.project
   subnets = [
     {
       subnet_name   = "gaurav-subnet"
       subnet_ip     = "10.100.10.0/24"
-      subnet_region = var.region
+      subnet_region = "us-central1"
     }
   ]
 }
